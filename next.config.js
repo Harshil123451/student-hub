b14c42a2578b1c8b585d741fe042a597d3b195c7
Harshil123451 <kri.chh.rt21@dypatil.edu>
2025-04-17 23:39:47 +0530

    Add Webpack alias configuration in next.config.js for improved module resolution. This change allows for easier imports using the '@' alias, enhancing code organization and maintainability.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   images: {
     domains: ['i.redd.it'],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
